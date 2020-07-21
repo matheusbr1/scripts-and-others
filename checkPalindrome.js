553407daa7ff37f282d6911f5bcb8d8e65ff9068
Matheus Baron <matheusbaron10@gmail.com>
2020-07-21 12:57:32 -0300

    Code Signal

---
+++ checkPalindrome.js
@@ -0,0 +1,17 @@
+function checkPalindrome(inputString) {
+    let output = '', string = []
+
+    for (i = 0; i < inputString.length; i++) {
+        string.unshift(inputString.substring(i, i + 1))
+    }
+
+    for (i = 0; i < string.length; i++) {
+        output += string[i]
+    }
+
+    return (output === inputString)
+}
+
+const input = 'matheus'
+
+checkPalindrome(input)
